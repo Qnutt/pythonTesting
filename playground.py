d6c057402e2470b942142133ce2bd1a690218e3c
Quinn Nutter <qnutter@gmail.com>
2025-07-20 22:29:55 -0400

    Initalizing playground testing file. Like other projects, this file is for small testing and messing around

---
+++ playground.py
@@ -0,0 +1,9 @@
+#FILE USED FOR TESTING AND PLAYING AROUND. NOTHING SERIOUS
+def testing(x) :
+  if x>0 :
+    return print("{x} is bigger than 0")
+  else:
+    return print('you suck')
+
+
+testing(5)
