94640ae45e7efc13583add625e740c94e1d1849e
Quinn Nutter <qnutter@gmail.com>
2025-07-21 23:48:10 -0400

    Created a virtual environment for my testing project Worked on a Codewars question

--- playground.py
+++ playground.py
@@ -6,4 +6,21 @@ def testing(x) :
     return print('you suck')
 
 
-testing(5)
+#testing(5)
+
+def findNeedle(junk) :
+
+  return print(f"Found the needle in position {junk.index('needle')}")
+
+example = ['3', '123124234', None, 'needle', 'world', 'hay', 2, '3', True, False]
+#findNeedle(example)
+
+def stray(arr) :
+  y = 0
+  x = arr[y]
+  while arr.count(x) >= 2:
+    y= y+1
+    x=arr[y]
+  return print(x)
+
+stray([13, 13, 13, 5, 13])
